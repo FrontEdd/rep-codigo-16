8d74e6f1a641686785f203691b3c0438f41f36ce
FrontEdd <edgar@urbaner.com>
2022-06-24 19:17:43 -0500

    Implementamos JS para el uso de funciones

---
+++ sem4/js/day5.js
@@ -0,0 +1,20 @@
+// ** Una FUNCTION es un bloque de código que realiza una acción
+
+// La sintáxis de una <function> se divide en 3:
+
+function nombreDeLaFuncion() {
+    const alumnos = [
+        'Pepe',
+        'Juan',
+        'Maria',
+        'Luisa',
+        'Lex',
+        'Lucas',
+        'Klark'
+    ]
+    for (const a of alumnos) {
+        console.log(a)
+    }
+}
+// Llamamos a la función:
+nombreDeLaFuncion()
