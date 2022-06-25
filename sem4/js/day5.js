ff47167f885383df490f6b1f0f10b5e55b65aca6
FrontEdd <edgar@urbaner.com>
2022-06-24 19:51:04 -0500

    Agregamos ejercicio con el uso de funciones y parametros

--- sem4/js/day5.js
+++ sem4/js/day5.js
@@ -18,3 +18,39 @@ function nombreDeLaFuncion() {
 }
 // Llamamos a la función:
 nombreDeLaFuncion()
+
+// Una función que retorne un valor
+// ** Existen 2 clases de funciones
+// ** Funciones IMPURAS => que no retorna un valor
+// ** Funciones PURAS => que retorna un valor
+
+function cuboDeUnNumero() {
+    return Math.pow(10, 3);
+}
+
+const result = cuboDeUnNumero();
+console.log('Resultado: ', result)
+
+// Una funcionalidad es poder enviar valores a las funciones
+// Tambien se pueden enviar 'parametros' y podemos enviar n params
+
+function potenciaDeUnNumero(numero, potencia) {
+    return Math.pow(numero, potencia);
+}
+
+const ejem1 = potenciaDeUnNumero(4, 3);
+const ejem2 = potenciaDeUnNumero(6, 2);
+
+console.log('Ejemplo1: ', ejem1)
+console.log('Ejemplo2: ', ejem2)
+
+// Ejercicio: 
+// Implementar un algoritmo que reciba 2 argumentos y los sume, el resultado se deberá imprimir en pantalla.
+
+function suma(x, y) {
+    return x + y;
+}
+
+const sumatoria = suma(8, 9)
+console.log(sumatoria)
+document.write('La sumatoria de 8 y 9 es: ' + sumatoria)
