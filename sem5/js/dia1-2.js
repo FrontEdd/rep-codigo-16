1acf131378c24f0b1c56a974d6049225d2c25d74
FrontEdd <edgar@urbaner.com>
2022-06-27 22:14:28 -0500

    Agregamos comentarios de los pasos para crear OBJETOS desde un formulario

--- sem5/js/dia1-2.js
+++ sem5/js/dia1-2.js
@@ -29,24 +29,39 @@ const variableH1 = document.querySelector("#h1-test");
 variableH1.onclick = () => {
     console.log("Click desde el h1");
     variableH1.innerText = "Cambiando el texto";
-  };
+};
 
-
+// Creando OBJETOS desde formulario
+// Vamos a captura los datos de nuestro formulario y guardarlo
+// en un array de objetos
   const users = [];
   const form = document.querySelector("#form-store-user");
 
+// vamos agregar el evento onsubmit
+
+//! NOTA el evento onsubmit tiene una prepiedad
+//! la cual nos da un parametro que esta implicito
+//! este parametro contiene la informacion del evento
+//! y se suele event o e
 
   form.onsubmit = (e) => {
-    
+    // * NOTA Un formulario al tener el evento submit va a recargar la pagina
+    //? Para evitar que el evento onsubmit recargue la pagina podemos usar el
+    //? evento.preventDefault() esta funcion bloquea esa accion de recargar la pagina
 
     e.preventDefault();
+    //!Nota La informacion de los inputs siempre estara en el target
+
     console.log(e.target);
 
-
+    // * Existe la clase FormData
     const formData = new FormData(e.target);
+
+    // vamos a crar un objeto user
     const user = {};
 
     for (const [key, value] of formData.entries()) {
+        // en el for vamos a agregar los campos a user
         user[key] = value;
         console.log(user[key])
     }
