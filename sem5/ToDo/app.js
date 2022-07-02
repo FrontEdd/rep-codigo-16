2115409f8bfe011e6b8087f820f8fc4bf5405f06
FrontEdd <edgar@urbaner.com>
2022-07-01 20:36:20 -0500

    Reto ToDO list - implementamos codigo para realizar acciones mediante checkbox

--- sem5/ToDo/app.js
+++ sem5/ToDo/app.js
@@ -1,40 +1,3 @@
-class Task {
-
-    constructor(_text) {
-        this.id = '_' + Math.random().toString(36).slice(2);
-        this.text = _text;
-        this.status = 'todo'; // done | delete
-        this.created_at = new Date();
-        // this.priority = 'high';
-        this.done_at = null;
-        this.deleted_at = null;
-    }
-
-    done() {
-        this.status = 'done';
-        this.done_at = new Date(); // fecha y hora actual.
-        document.querySelector("#" + this.id).classList.add('done');
-    }
-
-    delete() {
-        this.status = 'delete';
-        this.deleted_at = new Date();
-        document.querySelector("#" + this.id).classList.add('delete');
-    }
-
-    createElement() {
-        const element = document.createElement('p');
-        element.setAttribute('id', this.id);
-        element.setAttribute('class', 'task');
-        element.innerHTML = `
-            <input type="checkbox" onchange="checkTask(this);">
-            <span>${this.text}</span>
-            <a onclick="deleteTask(this);">❌</a>
-        `;
-        return element; // elemento HTML
-    }
-}
-
 // DOM:
 const listTasks = document.querySelector("#list");
 const inputTask = document.querySelector('#input-newtask');
@@ -73,4 +36,85 @@ function deleteTask(anchor) {
     task.delete();
 }
 
+// ! Código extenso:
+// const chxTaskDone = document.querySelector("#chx_task_done");
+// const chxTaskDelete = document.querySelector("#chx_task_delete");
+// const chxTaskTodo = document.querySelector("#chx_task_todo");
 
+// // Checkbox tiene el evento onchange
+// chxTaskDone.onchange = function () {
+//   const taskTodo = document.querySelectorAll(".todo");
+//   const taskDelete = document.querySelectorAll(".delete");
+//   console.log(taskTodo);
+//   if (this.checked) {
+//     // recurden que taskTodo es una lista de elementos HTML
+//     taskTodo.forEach((todo) => (todo.style.display = "none"));
+//     taskDelete.forEach((task) => (task.style.display = "none"));
+//     // vamos a mostrar solo la tareas terminadas
+//     // por ende vamos a buscar la tareas que tengan la clase todo y la clase delete
+//     // y las vamos a ocultar
+//     // querySelectarAll
+//   } else {
+//     taskTodo.forEach((todo) => (todo.style.display = "block"));
+//     taskDelete.forEach((task) => (task.style.display = "block"));
+//   }
+// };
+
+// chxTaskDelete.onchange = function () {
+//   const taskTodo = document.querySelectorAll(".todo");
+//   const taskDone = document.querySelectorAll(".done");  
+
+//   if (this.checked) {
+//     taskTodo.forEach((todo) => (todo.style.display = "none"));
+//     taskDone.forEach((done) => (done.style.display = "none"));
+//   } else {
+//     taskTodo.forEach((todo) => (todo.style.display = "block"));
+//     taskDone.forEach((done) => (done.style.display = "block"));
+//   }
+// };
+
+// chxTaskTodo.onchange = function () {
+//   const taskDone = document.querySelectorAll(".done");
+//   const taskDelete = document.querySelectorAll(".delete");
+
+//   if (this.checked) {
+//     taskDone.forEach((done) => (done.style.display = "none"));
+//     taskDelete.forEach((task) => (task.style.display = "none"));
+//   } else {
+//     taskDone.forEach((done) => (done.style.display = "block"));
+//     taskDelete.forEach((task) => (task.style.display = "block"));
+//   }
+// };
+
+
+// ? Código reducido:
+
+const chxTaskDone = document.querySelector("#chx_task_done");
+const chxTaskDelete = document.querySelector("#chx_task_delete");
+const chxTaskTodo = document.querySelector("#chx_task_todo");
+
+// Checkbox tiene el evento onchange
+chxTaskDone.onchange = function () {
+  showOrHideElement(this.checked, ".todo", ".delete");
+};
+
+chxTaskDelete.onchange = function () {
+  showOrHideElement(this.checked, ".todo", ".done");
+};
+
+chxTaskTodo.onchange = function () {
+  showOrHideElement(this.checked, ".delete", ".done");
+};
+
+function showOrHideElement(checked, typeOne, typeTwo) {
+  const elementsOne = document.querySelectorAll(typeOne);
+  const elementsTwo = document.querySelectorAll(typeTwo);
+
+  if (checked) {
+    elementsOne.forEach((todo) => (todo.style.display = "none"));
+    elementsTwo.forEach((task) => (task.style.display = "none"));
+  } else {
+    elementsOne.forEach((todo) => (todo.style.display = "block"));
+    elementsTwo.forEach((task) => (task.style.display = "block"));
+  }
+}
