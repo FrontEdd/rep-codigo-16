de757c791d4c267dd8e32513ec370d846b879b2e
FrontEdd <edgar@urbaner.com>
2022-07-01 20:45:13 -0500

    Reto ToDO list - mejoramos código en la clase Task

--- sem5/ToDo/task.js
+++ sem5/ToDo/task.js
@@ -10,31 +10,51 @@ class Task {
         this.deleted_at = null;
     }
 
+    // done() {
+    //     this.status = "done";
+    //     this.done_at = new Date(); // fecha y hora actual.
+    //     const element = document.querySelector("#" + this.id);
+    //     element.classList.add("done");
+    //     element.classList.remove("todo");
+    //   }
+    
+    //   delete() {
+    //     this.status = "delete";
+    //     this.deleted_at = new Date();
+    //     const element = document.querySelector("#" + this.id);
+    //     element.classList.add("delete");
+    //     element.classList.remove("todo");
+    //   }
+    
+    changeStatus(status) {
+        this.status = status;
+    
+        status === "delete"
+          ? (this.deleted_at = new Date())
+          : (this.done_at = new Date());
+    
+        const element = document.querySelector("#" + this.id);
+        element.classList.add(status);
+        element.classList.remove("todo");
+      }
+    
     done() {
-        this.status = "done";
-        this.done_at = new Date(); // fecha y hora actual.
-        const element = document.querySelector("#" + this.id);
-        element.classList.add("done");
-        element.classList.remove("todo");
-      }
-    
-      delete() {
-        this.status = "delete";
-        this.deleted_at = new Date();
-        const element = document.querySelector("#" + this.id);
-        element.classList.add("delete");
-        element.classList.remove("todo");
-      }
-    
-      createElement() {
-        const element = document.createElement("p");
-        element.setAttribute("id", this.id);
-        element.setAttribute("class", "task todo");
-        element.innerHTML = `
-                <input type="checkbox" onchange="checkTask(this);">
-                <span>${this.text}</span>
-                <a onclick="deleteTask(this);">❌</a>
-            `;
-        return element; // elemento HTML
-      }
+    this.changeStatus("done");
+    }
+
+    delete() {
+    this.changeStatus("delete");
+    }
+
+    createElement() {
+    const element = document.createElement("p");
+    element.setAttribute("id", this.id);
+    element.setAttribute("class", "task todo");
+    element.innerHTML = `
+            <input type="checkbox" onchange="checkTask(this);">
+            <span>${this.text}</span>
+            <a onclick="deleteTask(this);">❌</a>
+        `;
+    return element; // elemento HTML
+    }
 }
