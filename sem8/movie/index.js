ce5e07c48f9eda6b9aa667aec33b1ec045c753b5
FrontEdd <edgar@urbaner.com>
2022-07-18 22:58:04 -0500

    Agregamos badge para identificar programType en el listado de peliculas

--- sem8/movie/index.js
+++ sem8/movie/index.js
@@ -7,12 +7,17 @@ function renderCards(movies) {
     containerMovies.innerHTML += `
         <div class="col">
           <div class="card my-3" >
-            <img
-              src="${movie.images["Poster Art"].url}"
-              class="card-img-top"
-              onerror="this.onerror=null;this.src='https://icon-library.com/images/no-camera-icon/no-camera-icon-5.jpg';"
-              alt=""
-            />
+            <div class="into-photo">
+              <span class="badge text-bg-${
+                movie.programType === "series" ? "success" : "warning"
+                  }">${movie.programType}</span>
+              <img
+                src="${movie.images["Poster Art"].url}"
+                class="card-img-top"
+                onerror="this.onerror=null;this.src='https://i.quotev.com/img/q/u/15/12/25/78a31e5f80-imag.jpg';"
+                alt=""
+              />
+            </div>
             <div class="card-body">
               <div class="card-title">${movie.title}</div>
               <p class="card-text">
