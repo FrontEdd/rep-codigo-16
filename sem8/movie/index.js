67d51a29d07d5d74a0e5091468502fcd96247183
FrontEdd <edgar@urbaner.com>
2022-07-18 22:13:19 -0500

    Implementamos llamado de movie-api para mostrar cards de peliculas

--- sem8/movie/index.js
+++ sem8/movie/index.js
@@ -1,12 +1,37 @@
+const containerMovies = document.querySelector("#container-movies");
+
+function renderCards(movies) {
+  containerMovies.innerHTML = "";
+  movies.forEach((movie) => {
+
+    containerMovies.innerHTML += `
+        <div class="col-12 col-sm-4 col-md-3 col-lg-3">
+          <div class="card my-3" >
+            <img
+              src="${movie.images["Poster Art"].url}"
+              class="card-img-top"
+              alt=""
+            />
+            <div class="card-body">
+              <div class="card-title">${movie.title}</div>
+              <p class="card-text">
+               ${movie.description}
+              </p>
+            </div>
+          </div>
+        </div>
+    `;
+  });    
+}
+
+
 const url = "https://static.rviewer.io/challenges/datasets/dreadful-tomatoes/data.json";
-let movies = null;
 
 fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data));
-console.log("movies: ", movies);
-// console.log(peticion);
+    .then((data) => renderCards(data.entries));
 
+// console.log(peticion);
 // Recordar que fetch retorna una promesa, esta tiene 2 escenarios:
 //  ?resolve (resolver) => ocurre cuando la peticiónn esta ok
 //  !reejct (rechazar) => es cuando algo fallo
