92640a16c995ac83da8fb5ab612b778210b770f4
FrontEdd <edgar@urbaner.com>
2022-07-23 12:22:53 -0500

    Agregamos request y response mediante PUT para actualizar tareas con node

--- sem8/todo-api/index.js
+++ sem8/todo-api/index.js
@@ -38,8 +38,29 @@ app.post("/create-task", (request, response) => {
     todoListArray.push(task)
 
     response.json({
-        task,
+        tarea: task,
     });
+});
+
+app.put("/update-task/:id", (request, response) => {
+// Viene en el params
+// ! Todo lo que viene de params es un 'string'
+    const id = request.params.id
+    // Lo que el cliente envie estara en el body
+    const data = request.body
+    // ? 1- Buscar la tarea:
+    const taskIndex = todoListArray.findIndex(
+        (todoTask) => todoTask.id === Number(id)
+    )
+
+    todoListArray[taskIndex] = { 
+        ...todoListArray[taskIndex],
+        ...data 
+    }
+
+    return response.json({
+        tarea: todoListArray[taskIndex],
+    })
 });
 
 // Para poder visualizar desde el navegador vamos a habilitar un puerto
